c4623dd732e0724d95ed6dd8651e25cc51ddf05d
kunitoki <kunitoki@gmail.com>
2014-07-27 11:06:01 +0200

    - fixed setup python compatibilities

--- setup.py
+++ setup.py
@@ -36,7 +36,9 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
         'Environment :: Web Environment',
         'Topic :: Software Development',
     ]
